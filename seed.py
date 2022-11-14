bfd27982073f442b430e0547395730b007a82fc9
anela <anela89@gmail.com>
2022-11-14 13:53:10 -0800

    File to seed database

---
+++ seed.py
@@ -0,0 +1,24 @@
+from app import app
+from models import db, Pet
+
+db.drop_all()
+db.create_all()
+
+p1 = Pet(
+    name='Gus',
+    species='domestic shorthair',
+    photo_url='',
+    age='adult',
+    notes='A very grumpy boy.',
+    available=True,
+)
+
+p2 = Pet(
+    name='Arlo'
+    species='Canis familiaris',
+    age='young',
+    available=False,
+)
+
+db.session.add_all([p1, p2])
+db.session.commit()
