18601a5def43ba5d36d2051b223adefa720bcb2a
anela <anela89@gmail.com>
2022-11-14 17:01:13 -0800

    Created wtform for adding a new pet

---
+++ forms.py
@@ -0,0 +1,47 @@
+"""Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, URL, Optional
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField(
+        "Pet name",
+        validators=[
+            InputRequired(),
+            Length(max=50, message='Name must be less than %(max)d characters.')
+        ],
+    )
+    species = StringField(
+        "Pet species",
+        validators=[
+            InputRequired(),
+        ],
+    )
+    photo_url = StringField(
+        "Photo of pet",
+        validators=[
+            Optional(),
+            URL(),
+        ],
+    )
+    age = SelectField(
+        "Pet Age",
+        choices=[
+            ('baby', 'Baby'),
+            ('young', 'Young'),
+            ('adult', 'Adult'),
+            ('senior', 'Senior'),
+        ],
+        validators=[
+            InputRequired(),
+        ],
+    )
+    notes = TextAreaField(
+        "Notes",
+        validators=[
+            Optional(),
+        ],
+    )
