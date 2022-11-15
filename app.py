3178633a27889452a0df95e9b98e9ace3a554d6b
anela <anela89@gmail.com>
2022-11-14 17:02:12 -0800

    Created route to display the form to add a new pet

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 from flask import Flask, url_for, render_template, redirect, flash, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
@@ -24,3 +25,25 @@ def homepage_index():
     pets = Pet.query.all()
 
     return render_template('pets/list.html', pets=pets)
+ 
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    """Pet add form; handle creating a new pet."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        # create a new Pet model
+        # add to db
+        # commit to db
+
+        return redirect('/add')
+
+    else:
+        return render_template('pets/add.html', form=form)
